716eebcedf4a09924580aa65c555617e8f3d201c
Plenti Stack <147325282+plentistack@users.noreply.github.com>
2023-11-18 09:49:48 +0000

    add 02-react-state-and-props

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# 01-react-first-component
+# 02-react-state-and-props
 
 for more information see:
-- [Your first React Components and JSX: A Beginner’s Guide](https://plentistack.com/your-first-react-components-and-jsx-a-beginners-guide/?utm_source=github&utm_medium=website&utm_campaign=blog-post)
-- [Visit live sample](https://01-react-first-component.vercel.app/)
-
-[![Edit react](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/plentistack/react/tree/01-react-first-component/?fontsize=14&hidenavigation=1&theme=dark)
+- [Understanding the difference between State and Props in React](https://plentistack.com/understanding-the-difference-between-state-and-props-in-react/?utm_source=github&utm_medium=website&utm_campaign=blog-post)
 
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,123 @@
-import MyComponent from "./MyComponent";
+import React, { useState } from "react";
+import "./styles.css"; // Import the main styling file
 
-function App() {
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <MyComponent
-        greeting="Hey there!"
-        message="Welcome to the world of React."
+    <div className="counter-container">
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+const GreetUser = (props) => {
+  return <p className="greet-user">Hello, {props.name}!</p>;
+};
+
+const DynamicMessage = ({ initialMessage }) => {
+  const [message, setMessage] = useState(initialMessage);
+
+  const updateMessage = () => {
+    setMessage("New Message!");
+  };
+
+  return (
+    <div className="dynamic-message-container">
+      <p className="dynamic-message">{message}</p>
+      <button className="update-message-button" onClick={updateMessage}>
+        Update Message
+      </button>
+    </div>
+  );
+};
+
+const UserForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <form className="user-form">
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+      </label>
+    </form>
+  );
+};
+
+const Card = ({ title, content }) => {
+  return (
+    <div className="card">
+      <h2 className="card-title">{title}</h2>
+      <p className="card-content">{content}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      {/* Example 1: State */}
+      <h2>Example 1: State</h2>
+      <Counter />
+
+      {/* Example 2: Props */}
+      <h2>Example 2: Props</h2>
+      <GreetUser name="John" />
+
+      {/* Example 3: Integrating State and Props */}
+      <h2>Example 3: Integrating State and Props</h2>
+      <DynamicMessage initialMessage="Hello, React!" />
+
+      {/* Advanced Example 1: Advanced State Management */}
+      <h2>Example 4: State Management</h2>
+      <UserForm />
+
+      {/* Advanced Example 2: Props in Action */}
+      <h2>Example 5: Props in Action</h2>
+      <Card
+        title="React Mastery"
+        content="Unlock the power of React components."
+      />
+      <Card
+        title="Data Visualization"
+        content="Transforming complex data into insightful visuals."
       />
     </div>
   );
-}
+};
 
 export default App;
